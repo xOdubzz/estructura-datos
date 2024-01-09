c692da07fac4bcf719af456ef74e4daebb5db984
Irving <irving.spindola@unimilenium.edu.mx>
2024-01-09 10:41:24 -0600

    primer commit

---
+++ tiposEStructurasDatos.py
@@ -0,0 +1,14 @@
+#estructura lineal homogenea
+estructuraLineal = [2,4,5,7]
+
+print(estructuraLineal)
+
+
+#estructuras lineales heterogeneas
+
+estructuraHeterogenea = [1, "1", "hola", True, 5.2]
+
+print(estructuraHeterogenea[2])
+
+print("EL array tiene un tamano de", len(estructuraLineal))
+print("El array tiene un tamano de", len(estructuraHeterogenea))
